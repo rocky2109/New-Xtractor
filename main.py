9c298c4133cacc7a0a0b3d06f494f076818f13c2
rocky2109 <rockys.2109rs@gmail.com>
2025-07-11 10:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -615,14 +615,17 @@ async def txt_handler(client: Client, m: Message):
         )
      )   
     
-@bot.on_message(filters.command(["xtract3"]) )
+@bot.on_message(filters.command(["xtract"]) )
 async def txt_handler(bot: Client, m: Message):  
     if m.chat.id not in AUTH_USERS and m.chat.id not in CHANNELS_LIST:
         print(f"User ID not in AUTH_USERS", m.chat.id)
         print(f"Channel ID not in CHANNELS_LIST", m.chat.id)
-        await m.reply_text(f"<blockquote>__**Oopss! You are not a Premium member** __\n__**PLEASE /upgrade YOUR PLAN**__\n__**Send me your user id for authorization**__\n__**Your User id**__ - `{m.chat.id}`</blockquote>\n")
+        await m.reply_text(f"<blockquote>❌ You are not authorized to use this command. I just follow my Boss commands only 🫠</blockquote>\n")
         return
-    editable = await m.reply_text(f"**🔹Hi I am Poweful TXT Downloader📥 Bot.\n🔹Send me the txt file and wait.\n\n<blockquote><b>𝗡𝗼𝘁𝗲:\nAll input must be given in 20 sec</b></blockquote>**")
+    editable = await m.reply_text(
+        "**All Set Sir 🫡**\n"
+        "<blockquote><b>Just Send Me txt File & I will handle it automatically until it's done 😊</b></blockquote>"
+    )
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await bot.send_document(OWNER, x)
